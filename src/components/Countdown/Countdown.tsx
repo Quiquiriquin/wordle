fb068060aab61e04fe1a501d1506aad05f92af64
Enrique Alvarez <enrique@teametapp.com>
2023-10-16 14:15:32 -0600

    Message before getting the word

--- src/components/Countdown/Countdown.tsx
+++ src/components/Countdown/Countdown.tsx
@@ -3,18 +3,20 @@ import { GeneralContext } from '../../context/GeneralContext'
 
 const Countdown = () => {
   const { minutes, seconds, word } = useContext(GeneralContext)
-  if (minutes + seconds <= 0) {
+  if (minutes + seconds <= 0 && minutes + seconds >= -8) {
     return (
       <span>
         La palabra era <strong>{word.toUpperCase()}</strong>
       </span>
     )
+  } else if (minutes + seconds <= 0 && minutes + seconds < -8) {
+    return <span>¿Listo?</span>
   }
 
   return (
     <div>
       <label className='font-bold text-lg'>
-        {minutes > 10 ? minutes : `0${minutes}`}: {seconds > 10 ? seconds : `0${seconds}`}
+        {minutes >= 10 ? minutes : `0${minutes}`}: {seconds >= 10 ? seconds : `0${seconds}`}
       </label>
     </div>
   )
